d78401f9e334ffb1df06bb5d1f71493e2deb6019
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-31 03:05:25 +0530

    add a task to the todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState('');
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Task..."
+          onChange={(e) => setCurrentTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <hr />
     </div>
   );
 }
