696279397f630e0caef02e5f93c18c935c24b2c7
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-31 04:01:32 +0530

    completing a task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState('');
 
+  const inputTask = useRef(null);
+
   const addTask = () => {
-    setTodoList([...todoList, currentTask]);
+    setTodoList([...todoList, { task: currentTask, completed: false }]);
+    inputTask.current.value = '';
+    setCurrentTask('');
+  };
+
+  const deleteTask = (taskToDelete) => {
+    setTodoList(todoList.filter((task) => task.task !== taskToDelete));
+  };
+
+  const completeTask = (taskToComplete) => {
+    setTodoList(
+      todoList.map((task) => {
+        return task.task === taskToComplete
+          ? { task: taskToComplete, completed: true }
+          : { task: task.task, completed: task.completed };
+      })
+    );
   };
   return (
     <div className="App">
@@ -16,13 +34,25 @@ function App() {
           type="text"
           placeholder="Task..."
           onChange={(e) => setCurrentTask(e.target.value)}
+          ref={inputTask}
         />
         <button onClick={addTask}>Add Task</button>
       </div>
       <hr />
       <ul>
         {todoList.map((todo, key) => {
-          return <li key={key}>{todo}</li>;
+          return (
+            <div id="task">
+              <li key={key}>{todo.task}</li>
+              <button onClick={() => completeTask(todo.task)}>Completed</button>
+              <button onClick={() => deleteTask(todo.task)}>X</button>
+              {todo.completed ? (
+                <h1>Task Completed</h1>
+              ) : (
+                <h1>Task Not Completed</h1>
+              )}
+            </div>
+          );
         })}
       </ul>
     </div>
